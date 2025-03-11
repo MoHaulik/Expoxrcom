f84b826003b41df44aa3f72f9e543d5a427dae89
MoHaulik <88561887+MoHaulik@users.noreply.github.com>
2025-03-11 16:23:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// Custom interactivity can be added here
-document.addEventListener('DOMContentLoaded', () => {
-  console.log("ExpoXR website loaded successfully.");
+document.addEventListener("DOMContentLoaded", function() {
+  const mobileMenu = document.querySelector('.mobile-menu');
+  const navLinks = document.querySelector('.nav-links');
+
+  mobileMenu.addEventListener('click', function() {
+    navLinks.classList.toggle('active');
+  });
 });
