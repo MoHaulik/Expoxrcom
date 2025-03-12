88895c8118fdd07d6c7c432df235cee94386db4a
MoHaulik <88561887+MoHaulik@users.noreply.github.com>
2025-03-12 20:32:04 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const mobileMenu = document.querySelector('.mobile-menu');
-  const navMenu = document.querySelector('.nav-menu');
-
-  mobileMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-});
